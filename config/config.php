b80672b00a4de5e490364c9e31fa9b6ee9b70adb
mortazavi <s.mohammad027@live.com>
2022-04-26 20:38:55 +0430

    config file updated;

--- config/config.php
+++ config/config.php
@@ -1,5 +1,7 @@
 <?php
 
 	return [
-
+		'wrappedBy' => \Illuminate\Database\Eloquent\Model::query(),
+		'relation'  => 'products',
+		'entity'    => \Illuminate\Database\Eloquent\Model::query(),
 	];
