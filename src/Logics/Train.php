ef0e43cffb777be991144b92807408136e2ec7e0
mortazavi <s.mohammad027@live.com>
2022-04-26 20:59:45 +0430

    train implemented;

--- src/Logics/Train.php
+++ src/Logics/Train.php
@@ -1,21 +1,50 @@
 <?php
 
-    namespace Hans\Lilac\Logics;
+	namespace Hans\Lilac\Logics;
 
-    use App\Models\Shop\Set;
+	use Illuminate\Support\Arr;
 
-    class Train {
-        public function __invoke() {
-            $M  = Set::all();
-            $od = [];
-            $cd = [];
+	class Train {
+		private array $configuration;
 
-            foreach ( $M as $meal ) {
+		public function __invoke() {
+			$relation = $this->getConfig( 'relation' );
+			$M        = $this->getConfig( 'wrappedBy' )->all();
+			$OD       = [];
+			$CD       = [];
+			$PM       = null;
 
-                foreach ( $meal->products as $product ) {
-                    dd( $meal, $product );
-                }
+			foreach ( $M as $meal ) {
 
-            }
-        }
-    }
+				foreach ( $meal->{$relation} as $product ) {
+
+					if ( ! isset( $OD[ $product->id ] ) and ! isset( $CD[ $product->id ] ) ) {
+
+						$OD[ $product->id ] = 0;
+						$CD[ $product->id ] = [];
+
+					}
+					$OD[ $product->id ] = $OD[ $product->id ] + 1;
+
+					foreach ( $meal as $item ) {
+						if ( $item->is( $product ) ) {
+							continue;
+						}
+						if ( ! isset( $CD[ $product->id ][ $item->id ] ) ) {
+							$CD[ $product->id ][ $item->id ] = 0;
+						}
+						$CD[ $product->id ][ $item->id ] = $CD[ $product->id ][ $item->id ] + 1;
+					}
+
+				}
+
+				$PM = [ $OD, $CD ];
+			}
+
+			return $PM;
+		}
+
+		public function getConfig( string $key, $default = null ) {
+			return Arr::get( config( 'lilac' ), $key, $default );
+		}
+	}
