431124e3ecea83e04ef11727221170f895fad840
mortazavi <s.mohammad027@live.com>
2022-04-30 21:35:25 +0430

    recommend method fixed;

--- src/LilacService.php
+++ src/LilacService.php
@@ -3,7 +3,6 @@
 	namespace Hans\Lilac;
 
 	use Hans\Lilac\Logics\Train;
-	use Illuminate\Support\Arr;
 	use Illuminate\Support\Collection;
 
 	class LilacService {
@@ -24,20 +23,20 @@
 			$W  = [];
 			$OD = $PM[ 'OD' ];
 			$CD = $PM[ 'CD' ];
-
 			foreach ( $models as $inf ) {
-				foreach ( Arr::except( $CD[ $inf ], $models->pluck( 'id' ) ) as $item ) {
+				$inf = $inf->id;
+				foreach ( array_diff_key( $CD[ $inf ], $models->pluck( 'id' )->toArray() ) as $item => $count ) {
 					if ( ! isset( $P[ $item ] ) and ! isset( $W[ $item ] ) ) {
-						$P[ $item ] = [];
-						$W[ $item ] = [];
+						$P[ $item ] = 0;
+						$W[ $item ] = 0;
 					}
 					$p          = $CD[ $inf ][ $item ] / $OD[ $inf ];
 					$P[ $item ] = $P[ $item ] + $p;
 					$W[ $item ] = $W[ $item ] + $OD[ $inf ];
 				}
 			}
-			foreach ( $P as $item ) {
-				$RF[ $item ] = $P[ $item ] * $W[ $item ];
+			foreach ( $P as $item => $count ) {
+				$RF[ $item ] = $count * $W[ $item ];
 			}
 
 			return $RF;
