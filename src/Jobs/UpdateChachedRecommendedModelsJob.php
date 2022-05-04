618f7345b87ec2b96f7bf4cdf48748bcd5bc4a42
mortazavi <s.mohammad027@live.com>
2022-05-04 17:31:22 +0430

    A job created for updating cached data in background;

---
+++ src/Jobs/UpdateChachedRecommendedModelsJob.php
@@ -0,0 +1,31 @@
+<?php
+
+	namespace Hans\Lilac\Jobs;
+
+	use Illuminate\Bus\Queueable;
+	use Illuminate\Contracts\Queue\ShouldQueue;
+	use Illuminate\Foundation\Bus\Dispatchable;
+	use Illuminate\Queue\InteractsWithQueue;
+	use Illuminate\Queue\SerializesModels;
+
+	class UpdateChachedRecommendedModelsJob implements ShouldQueue {
+		use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+		/**
+		 * Create a new job instance.
+		 *
+		 * @return void
+		 */
+		public function __construct() {
+			//
+		}
+
+		/**
+		 * Execute the job.
+		 *
+		 * @return void
+		 */
+		public function handle() {
+			//
+		}
+	}
--- src/Traits/LilacTrait.php
+++ src/Traits/LilacTrait.php
@@ -3,13 +3,14 @@
 	namespace Hans\Lilac\Traits;
 
 	use Chelout\RelationshipEvents\Concerns\HasBelongsToManyEvents;
+	use Hans\Lilac\Jobs\UpdateChachedRecommendedModelsJob;
 
 	class LilacTrait {
 		use HasBelongsToManyEvents;
 
 		public function boot() {
 			static::belongsToManySynced( function( $relation, $parent, $ids ) {
-
+				UpdateChachedRecommendedModelsJob::dispatch($relation, $parent);
 			} );
 		}
 	}
