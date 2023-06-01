d2afed5abe7a93f3c710783939c9c8ab4bd9e293
mortazavi <s.mohammad027@live.com>
2023-06-01 11:42:12 +0330

    train model jobs created;

---
+++ src/Jobs/UpdateEntityTrainModel.php
@@ -0,0 +1,35 @@
+<?php
+
+	namespace Hans\Lilac\Jobs;
+
+	use Hans\Lilac\Facades\Lilac;
+	use Illuminate\Bus\Queueable;
+	use Illuminate\Contracts\Queue\ShouldQueue;
+	use Illuminate\Database\Eloquent\Model;
+	use Illuminate\Foundation\Bus\Dispatchable;
+	use Illuminate\Queue\InteractsWithQueue;
+	use Illuminate\Queue\SerializesModels;
+
+	class UpdateEntityTrainModel implements ShouldQueue {
+		use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+		/**
+		 * Create a new job instance.
+		 *
+		 * @return void
+		 */
+		public function __construct(
+			protected Model $model
+		) {
+
+		}
+
+		/**
+		 * Execute the job.
+		 *
+		 * @return void
+		 */
+		public function handle() {
+			Lilac::updateTrainModel( collect( [ $this->model ] ) );
+		}
+	}
